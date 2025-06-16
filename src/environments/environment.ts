c57648221caa9dba43ca1de5f2b9ce20255ab497
Hmayda <abdoessamadhmayda@gmail.com>
2025-06-16 10:53:22 +0100

    backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/talentConnect/api/'
+  apiUrl: 'https://talentconnectbackend-production.up.railway.app/talentConnect/api/'
 };
